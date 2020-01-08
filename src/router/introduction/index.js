dccd8b8ac76e359a836447b75a3bd235299e1308
kiddzz <zhukaida@hzzhebei.com>
2020-01-08 11:37:00 +0800

    图片修改

--- src/router/introduction/index.js
+++ src/router/introduction/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { animateScroll as scroll } from 'react-scroll'
-import headerImg from '@/assets/放心消费页面长兴.jpg'
-import headerImg2 from '@/assets/安吉、南浔、湖州 放心消费.jpg'
 import btnImg from '@/assets/introduction_btn.png'
+import homeImg from '@/assets/home.png'
 import './index.scss'
+
+
+const headerImg = 'http://static.zbszkj.com/images/%E6%94%BE%E5%BF%83%E6%B6%88%E8%B4%B9%E9%A1%B5%E9%9D%A2%E9%95%BF%E5%85%B4.jpg'
+const headerImg2 = 'http://static.zbszkj.com/images/%E6%94%BE%E5%BF%83%E6%B6%88%E8%B4%B9%E9%A1%B5%E9%9D%A2%E6%B5%99%E5%8C%97.jpg'
 
 export default class ActivityA extends React.Component {
   state = {
@@ -12,6 +15,7 @@ export default class ActivityA extends React.Component {
   }
 
   componentDidMount() {
+    console.log(this.props.match)
     window.addEventListener('scroll', this.onScroll, true)
   }
 
@@ -39,22 +43,26 @@ export default class ActivityA extends React.Component {
     }
   }
 
+  toHome = () => {
+    window.wx.miniProgram.switchTab({
+      url: '/pages/mall/index'
+    })
+  }
+
   render() {
     const { btnShow } = this.state
     return (
       <div className='introduction' ref={a => (this.content = a)}>
         <img className='introduction-img' src={this.state.id == 1 ? headerImg : headerImg2} />
-        {btnShow && (
-          <div className='scroll-box'>
-            <img className='scroll-btn' src={btnImg} />
-            <div className='scroll-btns'>
-              <div onClick={() => this.scrollTo(0)}>1</div>
-              <div onClick={() => this.scrollTo(1)}>2</div>
-              <div onClick={() => this.scrollTo(2)}>3</div>
-              <div onClick={() => this.scrollTo(3)}>4</div>
-            </div>
+        <div className={`scroll-box ${btnShow ? '' : 'disabled'}`}>
+          <img className='scroll-btn' src={btnImg} />
+          <div className='scroll-btns'>
+            <div onClick={() => this.scrollTo(0)}>1</div>
+            <div onClick={() => this.scrollTo(1)}>2</div>
+            <div onClick={() => this.scrollTo(2)}>3</div>
+            <div onClick={() => this.scrollTo(3)}>4</div>
           </div>
-        )}
+        </div>
         <div className='introduction-btns'>
           <div onClick={() => this.scrollTo(0)}>1</div>
           <div onClick={() => this.scrollTo(1)}>2</div>
@@ -65,6 +73,9 @@ export default class ActivityA extends React.Component {
         <div className='introduction-href introduction-2'>2</div>
         <div className='introduction-href introduction-3'>3</div>
         <div className='introduction-href introduction-4'>4</div>
+        <div className='introduction-back' onClick={this.toHome}>
+          <img src={homeImg} />
+        </div>
       </div>
     )
   }
